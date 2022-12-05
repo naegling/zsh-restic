bbde1b1e2485aac71efb40b5918108e906f2c8d5
Richard Rutledge <richard@therutledge.com>
2022-12-05 17:35:26 -0500

    restored lazy generation

--- zsh-restic.plugin.zsh
+++ zsh-restic.plugin.zsh
@@ -6,8 +6,9 @@ fi
 # If the completion file doesn't exist yet, we need to autoload it and
 # bind it to `restic`. Otherwise, compinit will have already done that.
 if [[ ! -f "$ZSH_CACHE_DIR/completions/_restic" ]]; then
-  restic generate --zsh-completion "$ZSH_CACHE_DIR/completions/_restic" &>/dev/null
   typeset -g -A _comps
   autoload -Uz _restic
   _comps[restic]=_restic
 fi
+
+restic generate --zsh-completion "$ZSH_CACHE_DIR/completions/_restic" &>/dev/null &|
